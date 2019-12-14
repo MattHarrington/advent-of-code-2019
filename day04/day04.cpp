4eab34ec932a25ee01c9e296f795100a275f183c
Matt Harrington <MattHarrington@users.noreply.github.com>
2019-12-13 23:39:23 -0800

    Day 4 refactored

--- day04/day04.cpp
+++ day04/day04.cpp
@@ -1,10 +1,12 @@
 // https://adventofcode.com/2019/day/4
 
+#include <algorithm>
 #include <cassert>
 #include <iostream>
 #include <iterator>
 #include <string>
 
+// Alternative: use std::is_sorted()
 bool digits_are_increasing(int i) {
 	int first_digit{ i / 100'000 };
 	i %= 100'000;
@@ -21,15 +23,14 @@ bool digits_are_increasing(int i) {
 		&& (third_digit <= fourth_digit) && (fourth_digit <= fifth_digit) && (fifth_digit <= sixth_digit);
 }
 
-bool part1_has_repeated_digit(int i) {
+bool part1_has_pair(int i) {
 	std::string s{ std::to_string(i) };
-	for (auto it{ begin(s) }; it < std::prev(end(s)); ++it) {
-		if (*it == *(std::next(it))) return true;
-	}
-	return false;
+	auto it{ std::adjacent_find(begin(s), end(s)) };
+	return it != end(s);
 }
 
-bool part2_has_repeated_digit(int i) {
+// Alternative: count repeated digits & look for number of repeats == 2
+bool part2_has_pair(int i) {
 	std::string s{ std::to_string(i) };
 	// Check from beginning up to the last 2 positions
 	for (auto it{ begin(s) }; it < end(s) - 2; ++it) {
@@ -45,31 +46,31 @@ bool part2_has_repeated_digit(int i) {
 
 int main()
 {
-	assert(digits_are_increasing(111'111) && part1_has_repeated_digit(111'111));
+	assert(digits_are_increasing(111'111) && part1_has_pair(111'111));
 	assert(digits_are_increasing(223'450) == false);
-	assert(part1_has_repeated_digit(223'450));
+	assert(part1_has_pair(223'450));
 	assert(digits_are_increasing(123'789));
-	assert(part1_has_repeated_digit(123'789) == false);
+	assert(part1_has_pair(123'789) == false);
 
 	int part_one_answer{ 0 };
 	int part_two_answer{ 0 };
 
 	for (int i = 206'938; i <= 679'128; i++)
 	{
-		if (digits_are_increasing(i) && part1_has_repeated_digit(i))
+		if (digits_are_increasing(i) && part1_has_pair(i))
 		{
 			++part_one_answer;
 		}
-		if (digits_are_increasing(i) && part2_has_repeated_digit(i)) {
+		if (digits_are_increasing(i) && part2_has_pair(i)) {
 			++part_two_answer;
 		}
 	}
 	std::cout << "part_one_answer: " << part_one_answer << "\n";
 	assert(part_one_answer == 1653);
 
-	assert(part2_has_repeated_digit(112'233));
-	assert(part2_has_repeated_digit(123'444) == false);
-	assert(part2_has_repeated_digit(111'122));
+	assert(part2_has_pair(112'233));
+	assert(part2_has_pair(123'444) == false);
+	assert(part2_has_pair(111'122));
 
 	std::cout << "part_two_answer: " << part_two_answer << "\n";
 	assert(part_two_answer == 1133);
