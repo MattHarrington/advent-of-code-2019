47eed54d2e0aa580aa2c41614dd0d6a38d8ed363
Matt Harrington <MattHarrington@users.noreply.github.com>
2019-12-13 17:59:30 -0800

    Day 4 part 2 complete

--- day04/day04.cpp
+++ day04/day04.cpp
@@ -21,7 +21,7 @@ bool digits_are_increasing(int i) {
 		&& (third_digit <= fourth_digit) && (fourth_digit <= fifth_digit) && (fifth_digit <= sixth_digit);
 }
 
-bool has_repeated_digit(int i) {
+bool part1_has_repeated_digit(int i) {
 	std::string s{ std::to_string(i) };
 	for (auto it{ begin(s) }; it < std::prev(end(s)); ++it) {
 		if (*it == *(std::next(it))) return true;
@@ -29,48 +29,49 @@ bool has_repeated_digit(int i) {
 	return false;
 }
 
-//123'444
-bool part2_repeated_digit(int i) {
+bool part2_has_repeated_digit(int i) {
 	std::string s{ std::to_string(i) };
+	// Check from beginning up to the last 2 positions
 	for (auto it{ begin(s) }; it < end(s) - 2; ++it) {
-		if (*it == *(std::next(it)) && *it == *(it + 2)) it += 2;
-		else if (*it == *(std::next(it)) && (*it != *(it + 2))) return true;
+		if (*it == *(std::next(it)) && (*it != *(it + 2))) return true;
+		while (it < end(s) - 1 && *it == *(std::next(it))){
+			++it;
+		}
 	}
+	// Check last 2 positions
 	if (*(end(s) - 2) == *(end(s) - 1) && *(end(s) - 3) != *(end(s) - 2)) return true;
 	return false;
 }
 
 int main()
 {
-	assert(digits_are_increasing(111'111) && has_repeated_digit(111'111));
+	assert(digits_are_increasing(111'111) && part1_has_repeated_digit(111'111));
 	assert(digits_are_increasing(223'450) == false);
-	assert(has_repeated_digit(223'450));
+	assert(part1_has_repeated_digit(223'450));
 	assert(digits_are_increasing(123'789));
-	assert(has_repeated_digit(123'789) == false);
+	assert(part1_has_repeated_digit(123'789) == false);
 
 	int part_one_answer{ 0 };
 	int part_two_answer{ 0 };
 
 	for (int i = 206'938; i <= 679'128; i++)
 	{
-		if (digits_are_increasing(i) && has_repeated_digit(i))
+		if (digits_are_increasing(i) && part1_has_repeated_digit(i))
 		{
 			++part_one_answer;
 		}
-		if (digits_are_increasing(i) && part2_repeated_digit(i)) {
+		if (digits_are_increasing(i) && part2_has_repeated_digit(i)) {
 			++part_two_answer;
 		}
 	}
 	std::cout << "part_one_answer: " << part_one_answer << "\n";
 	assert(part_one_answer == 1653);
 
-	assert(part2_repeated_digit(112'233));
-	assert(part2_repeated_digit(123'444) == false);
-	assert(part2_repeated_digit(111'122));
+	assert(part2_has_repeated_digit(112'233));
+	assert(part2_has_repeated_digit(123'444) == false);
+	assert(part2_has_repeated_digit(111'122));
 
 	std::cout << "part_two_answer: " << part_two_answer << "\n";
+	assert(part_two_answer == 1133);
 }
-// 1446 too high
-// 478 too low
-// 538 too low
-// 1158 no
+
