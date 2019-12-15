24901c31eabafe426d9ee9acba6596876b5a3301
MattHarrington <MattHarrington@users.noreply.github.com>
2019-12-14 17:50:32 -0800

    Day 5 part 1 complete

--- day05/day05.cpp
+++ day05/day05.cpp
@@ -5,56 +5,73 @@
 #include <stdexcept>
 #include <vector>
 
-enum opcode {
+enum class opcode {
 	add = 1, multiply, input, output, halt = 99
 };
 
-std::vector<int> process(std::vector<int> input) {
-	size_t pos{ 0 };
-	while (input.at(pos) != halt) {
-		auto input1_pos{ input.at(pos + 1) };
-		auto input2_pos{ input.at(pos + 2) };
-		auto output_pos{ input.at(pos + 3) };
+enum class mode {
+	position, immediate
+};
 
-		switch (input.at(pos)) {
-		case add:
-			input.at(output_pos) = input.at(input1_pos) + input.at(input2_pos);
+std::vector<int> process(std::vector<int> program, int input) {
+	size_t ip{ 0 };
+	while (static_cast<opcode>(program.at(ip)) != opcode::halt) {
+		mode param1_mode{ mode::position };
+		mode param2_mode{ mode::position };
+		mode param3_mode{ mode::position };
+		if (program.at(ip) > 100) {
+			int modes{ program.at(ip) / 100 };
+			param1_mode = modes % 10 == 0 ? mode::position : mode::immediate;
+			modes /= 10;
+			param2_mode = modes % 10 == 0 ? mode::position : mode::immediate;
+			modes /= 10;
+			param3_mode = modes % 10 == 0 ? mode::position : mode::immediate;
+		}
+
+		opcode op{ program.at(ip) < 100 ? static_cast<opcode>(program.at(ip)) : static_cast<opcode>(program.at(ip) % 100) };
+
+		switch (op) {
+		case opcode::add:
+		{
+			auto param1{ param1_mode == mode::position ? program.at(program.at(ip + 1)) : program.at(ip + 1) };
+			auto param2{ param2_mode == mode::position ? program.at(program.at(ip + 2)) : program.at(ip + 2) };
+			auto param3{ param3_mode == mode::immediate ? program.at(program.at(ip + 3)) : program.at(ip + 3) }; // never in immediate mode for writes
+			program.at(param3) = param1 + param2;
+			ip += 4;
 			break;
-		case multiply:
-			input.at(output_pos) = input.at(input1_pos) * input.at(input2_pos);
+		}
+		case opcode::multiply:
+		{
+			auto param1{ param1_mode == mode::position ? program.at(program.at(ip + 1)) : program.at(ip + 1) };
+			auto param2{ param2_mode == mode::position ? program.at(program.at(ip + 2)) : program.at(ip + 2) };
+			auto param3{ param3_mode == mode::immediate ? program.at(program.at(ip + 3)) : program.at(ip + 3) }; // never in immediate mode for writes
+			program.at(param3) = param1 * param2;
+			ip += 4;
 			break;
-		//case input:
-		//	input.at(output_pos) = 
+		}
+		case opcode::input:
+		{
+			auto param1{ program.at(ip + 1) };
+			program.at(param1) = input;
+			ip += 2;
+			break;
+		}
+		case opcode::output:
+		{
+			auto param1{ param1_mode == mode::position ? program.at(program.at(ip + 1)) : program.at(ip + 1) };
+			std::cout << "output: " << param1 << "\n";
+			ip += 2;
+			break;
+		}
 		default:
 			throw std::logic_error("Bad opcode");
 		}
-		pos += 4;
 	}
-	return input;
+	return program;
 }
 
 int main() {
-	std::vector<int> ex1{ 1,9,10,3,2,3,11,0,99,30,40,50 };
-	std::vector<int> ex1_answer{ 3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50 };
-	assert(process(ex1) == ex1_answer);
-
-	std::vector<int> ex2{ 1,0,0,0,99 };
-	std::vector<int> ex2_answer{ 2,0,0,0,99 };
-	assert(process(ex2) == ex2_answer);
-
-	std::vector<int> ex3{ 2,3,0,3,99 };
-	std::vector<int> ex3_answer{ 2,3,0,6,99 };
-	assert(process(ex3) == ex3_answer);
-
-	std::vector<int> ex4{ 2,4,4,5,99,0 };
-	std::vector<int> ex4_answer{ 2,4,4,5,99,9801 };
-	assert(process(ex4) == ex4_answer);
-
-	std::vector<int> ex5{ 1,1,1,4,99,5,6,0,99 };
-	std::vector<int> ex5_answer{ 30,1,1,4,2,5,6,0,99 };
-	assert(process(ex5) == ex5_answer);
-
-	std::vector<int> input{ 3,225,1,225,6,6,1100,1,238,225,104,0,1102,59,58,224,1001,224,-3422,
+	std::vector<int> program{ 3,225,1,225,6,6,1100,1,238,225,104,0,1102,59,58,224,1001,224,-3422,
 		224,4,224,102,8,223,223,101,3,224,224,1,224,223,223,1101,59,30,225,1101,53,84,224,
 		101,-137,224,224,4,224,1002,223,8,223,101,3,224,224,1,223,224,223,1102,42,83,225,
 		2,140,88,224,1001,224,-4891,224,4,224,1002,223,8,223,1001,224,5,224,1,223,224,223,
@@ -89,34 +106,12 @@ int main() {
 
 	// Part 1
 
-	// Change input per the problem statement:
-	input.at(1) = 12;
-	input.at(2) = 2;
+	//auto output1{ process({1002}, 1) };
+	//auto ex1{ process({3,0,4,0,99}, 1) };
+	//auto ex2{ process({1002,4,3,4,33}, 1) };
 
-	std::vector<int> output1 = process(input);
-	const int part_one_answer = output1.at(0);
-	std::cout << "part_one_answer: " << part_one_answer << "\n";
-	assert(part_one_answer == 5534943);
-
-	// Part 2
-
-	int part_two_answer{ -1 };
-
-	for (int n{ 0 }; n < 100; ++n) {
-		input.at(1) = n;
-		for (int v{ 0 }; v < 100; ++v) {
-			input.at(2) = v;
-			std::vector<int> output2 = process(input);
-			if (output2.at(0) == 19690720) {
-				part_two_answer = 100 * n + v;
-				// Set n, v to break out of loops:
-				n = 5000;
-				v = 5000;
-			}
-		}
-	}
-	std::cout << "part_two_answer: " << part_two_answer << "\n";
-	assert(part_two_answer == 7603);
+	std::vector<int> output = process(program, 1);
 
 	return 0;
 }
+
