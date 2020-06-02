a56abf8920df1e582a5e2e6c6d5055092e2bfe62
Matt Harrington <MattHarrington@users.noreply.github.com>
2020-06-02 10:55:47 -0700

    Day 12 complete

--- day12/day12-tests/day12-tests.cpp
+++ day12/day12-tests/day12-tests.cpp
@@ -56,12 +56,12 @@ TEST_CASE("energy after 10 timesteps should be 179", "[part_one]") {
         Moon{{3,-6,1}, {3,2,-3}},
         Moon{{2,0,4}, {1,-1,-1}}
     };
-    int total_energy{ std::accumulate(begin(moons_after_10_timesteps), end(moons_after_10_timesteps),
-        0, [](int t, Moon m) {return t + get_total_energy(m); }) };
+    const int total_energy{ std::accumulate(begin(moons_after_10_timesteps), end(moons_after_10_timesteps),
+        0, [](int t, Moon m) noexcept {return t + get_total_energy(m); }) };
     REQUIRE(total_energy == 179);
 }
 
-TEST_CASE("sample moons should repeat x value at 17", "[part_two]") {
+TEST_CASE("sample moons should repeat x value at timestep 18", "[part_two]") {
     std::vector<Moon> sample_moons{
         Moon{{-1,0,2}, {0,0,0}},
         Moon{{2,-10,-7}, {0,0,0}},
@@ -72,13 +72,15 @@ TEST_CASE("sample moons should repeat x value at 17", "[part_two]") {
     do {
         sample_moons = timestep(sample_moons);
         ++step;
-    } while (sample_moons[0].position.x != -1 || sample_moons[1].position.x != 2
-        || sample_moons[2].position.x != 4 || sample_moons[3].position.x != 3);
+    } while (sample_moons.at(0).position.x != -1 || sample_moons.at(1).position.x != 2
+        || sample_moons.at(2).position.x != 4 || sample_moons.at(3).position.x != 3
+        || sample_moons.at(0).velocity.x_velocity == sample_moons.at(1).velocity.x_velocity ==
+        sample_moons.at(2).velocity.x_velocity == sample_moons.at(3).velocity.x_velocity == 0);
 
-    REQUIRE(step == 17);
+    REQUIRE(step == 18);
 }
 
-TEST_CASE("sample moons should repeat y value at 27", "[part_two]") {
+TEST_CASE("sample moons should repeat y value at timestep 28", "[part_two]") {
     std::vector<Moon> sample_moons{
         Moon{{-1,0,2}, {0,0,0}},
         Moon{{2,-10,-7}, {0,0,0}},
@@ -89,13 +91,15 @@ TEST_CASE("sample moons should repeat y value at 27", "[part_two]") {
     do {
         sample_moons = timestep(sample_moons);
         ++step;
-    } while (sample_moons[0].position.y != 0 || sample_moons[1].position.y != -10 ||
-        sample_moons[2].position.y != -8 || sample_moons[3].position.y != 5);
+    } while (sample_moons.at(0).position.y != 0 || sample_moons.at(1).position.y != -10 ||
+        sample_moons.at(2).position.y != -8 || sample_moons.at(3).position.y != 5
+        || sample_moons.at(0).velocity.y_velocity == sample_moons.at(1).velocity.y_velocity ==
+        sample_moons.at(2).velocity.y_velocity == sample_moons.at(3).velocity.y_velocity == 0);
 
-    REQUIRE(step == 27);
+    REQUIRE(step == 28);
 }
 
-TEST_CASE("sample moons should repeat z value at 43", "[part_two]") {
+TEST_CASE("sample moons should repeat z value at timestep 44", "[part_two]") {
     std::vector<Moon> sample_moons{
         Moon{{-1,0,2}, {0,0,0}},
         Moon{{2,-10,-7}, {0,0,0}},
@@ -106,15 +110,18 @@ TEST_CASE("sample moons should repeat z value at 43", "[part_two]") {
     do {
         sample_moons = timestep(sample_moons);
         ++step;
-    } while (sample_moons[0].position.z != 2 || sample_moons[1].position.z != -7 ||
-        sample_moons[2].position.z != 8 || sample_moons[3].position.z != -1);
+    } while (sample_moons.at(0).position.z != 2 || sample_moons.at(1).position.z != -7 ||
+        sample_moons.at(2).position.z != 8 || sample_moons.at(3).position.z != -1
+        || sample_moons.at(0).velocity.z_velocity == sample_moons.at(1).velocity.z_velocity ==
+        sample_moons.at(2).velocity.z_velocity == sample_moons.at(3).velocity.z_velocity == 0);
 
-    REQUIRE(step == 43);
+    REQUIRE(step == 44);
 }
 
 TEST_CASE("sample moons should repeat state at timestep 2722", "[part_two]") {
-    int repeat_timestep_x_y{ std::lcm(16,26) };
-    int repeat_timestep_xy_z{ std::lcm(repeat_timestep_x_y, 42) };
+    // 18, 28, and 44 are results from other TEST_CASEs
+    constexpr int repeat_timestep_x_y{ std::lcm(18,28) };
+    constexpr int repeat_timestep_xy_z{ std::lcm(repeat_timestep_x_y, 44) };
     REQUIRE(repeat_timestep_xy_z == 2772);
 }
 
--- day12/day12/day12.cpp
+++ day12/day12/day12.cpp
@@ -1,24 +1,81 @@
 // https://adventofcode.com/2019/day/12
 
-#include <cassert>
+#include <gsl/gsl_assert>
 #include <iostream>
 #include <numeric>
 #include "day12-lib.h"
 
 int main() {
-    std::vector<Moon> moons{
+    const std::vector<Moon> moons{
         Moon{ {12,0,-15},  {0,0,0} },
         Moon{ {-8,-5,-10}, {0,0,0} },
         Moon{ {7,-17,1},   {0,0,0} },
         Moon{ {2,-11,-6},  {0,0,0} }
     };
 
+    // Sample:
+    //const std::vector<Moon> moons{
+    //    Moon{{-1,0,2},    {0,0,0}},
+    //    Moon{{2,-10,-7},  {0,0,0}},
+    //    Moon{{4,-8,8},    {0,0,0}},
+    //    Moon{{3,5,-1},    {0,0,0}}
+    //};
+
+    // Sample:
+    //const std::vector<Moon> moons{
+    //    Moon{ {-8,-10,0}, {0,0,0} },
+    //    Moon{ {5,5,10},   {0,0,0} },
+    //    Moon{ {2,-7,3},   {0,0,0} },
+    //    Moon{ {9,-8,-3},  {0,0,0} }
+    //};
+
+    // Part 1
+
+    std::vector<Moon> part1_moons{ moons };
     for (int i{ 0 }; i < 1000; ++i) {
-        moons = timestep(moons);
+        part1_moons = timestep(part1_moons);
     }
 
-    const int part1_answer{ std::accumulate(begin(moons), end(moons), 0, [](int t, Moon m) {return t + get_total_energy(m); }) };
+    const int part1_answer{ std::accumulate(begin(part1_moons), end(part1_moons), 0,
+        [](int t, Moon m) noexcept {return t + get_total_energy(m); }) };
     std::cout << "part1_answer: " << part1_answer << "\n";
-    assert(part1_answer == 7636);
+    Ensures(part1_answer == 7636);
+
+    // Part 2
+
+    std::vector<Moon> part2_moons{ moons };
+    long long x_step{ 0 };
+    do {
+        part2_moons = timestep(part2_moons);
+        ++x_step;
+    } while (part2_moons.at(0).position.x != moons.at(0).position.x || part2_moons.at(1).position.x != moons.at(1).position.x
+        || part2_moons.at(2).position.x != moons.at(2).position.x || part2_moons.at(3).position.x != moons.at(3).position.x
+        || part2_moons.at(0).velocity.x_velocity == part2_moons.at(1).velocity.x_velocity ==
+        part2_moons.at(2).velocity.x_velocity == part2_moons.at(3).velocity.x_velocity == 0);
+
+    part2_moons = moons;
+    long long y_step{ 0 };
+    do {
+        part2_moons = timestep(part2_moons);
+        ++y_step;
+    } while (part2_moons.at(0).position.y != moons.at(0).position.y || part2_moons.at(1).position.y != moons.at(1).position.y ||
+        part2_moons.at(2).position.y != moons.at(2).position.y || part2_moons.at(3).position.y != moons.at(3).position.y
+        || part2_moons.at(0).velocity.y_velocity == part2_moons.at(1).velocity.y_velocity ==
+        part2_moons.at(2).velocity.y_velocity == part2_moons.at(3).velocity.y_velocity == 0);
+
+    part2_moons = moons;
+    long long z_step{ 0 };
+    do {
+        part2_moons = timestep(part2_moons);
+        ++z_step;
+    } while (part2_moons.at(0).position.z != moons.at(0).position.z || part2_moons.at(1).position.z != moons.at(1).position.z ||
+        part2_moons.at(2).position.z != moons.at(2).position.z || part2_moons.at(3).position.z != moons.at(3).position.z
+        || part2_moons.at(0).velocity.z_velocity == part2_moons.at(1).velocity.z_velocity ==
+        part2_moons.at(2).velocity.z_velocity == part2_moons.at(3).velocity.z_velocity == 0);
+
+    const long long repeat_timestep_x_y{ std::lcm(x_step,y_step) };
+    const long long part2_answer{ std::lcm(repeat_timestep_x_y, z_step) };
+    std::cout << "part2_answer: " << part2_answer << "\n";
+    Ensures(part2_answer == 281691380235984);
 }
 
